eb268f6f43139b137dfd6c2354002e56918a1b8e
SenatorDingo <abhayabhilash@duck.com>
2024-10-10 18:01:49 -0400

    AA - Upload Minimum Viable Product
    Changes:
    - Added a Minimum Viable Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [FormsModule]
 })
 export class AppComponent {
-  title = 'DuelScore';
+  team1Name = '';
+  team2Name = '';
+  team1Score = 0;
+  team2Score = 0;
+
+  lastAction: { team: number, change: number } | null = null;
+
+  updateScore(team: number, change: number) {
+    if (team === 1) {
+      this.team1Score += change;
+    } else if (team === 2) {
+      this.team2Score += change;
+    }
+
+    this.lastAction = { team, change };
+  }
+
+  undo() {
+    if (this.lastAction) {
+      this.updateScore(this.lastAction.team, -this.lastAction.change);
+      this.lastAction = null;
+    }
+  }
 }
